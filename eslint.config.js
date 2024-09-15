1fe88f6edc4548e74292ce9cea2a2a170e4b721e
l5 <277705+l5@users.noreply.github.com>
2024-09-16 08:53:37 +1200

    Updated eslint to include typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,35 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import { ESLint } from "eslint";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  new ESLint({
+    overrideConfig: {
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+      ],
+      env: {
+        browser: true,
+        es2021: true,
+        node: true
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module"
+      },
+      rules: {
+        // Add your custom rules here
+      }
+    },
+    extensions: [".ts"]
+  })
 ];
