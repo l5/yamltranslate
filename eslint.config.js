04ea77ef19721a20d394610bef37a73f98e7d222
l5 <277705+l5@users.noreply.github.com>
2024-09-16 09:53:41 +1200

    added jest to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,16 @@
-import globals from "globals";
-import js from "@eslint/js";
 import ts from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
+import globals from "globals";
+import { Linter } from "eslint";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: {
-      globals: globals.node,
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
       parser: parser,
       parserOptions: {
         ecmaVersion: 2021,
@@ -18,7 +21,7 @@ export default [
       "@typescript-eslint": ts
     },
     rules: {
-      ...js.configs.recommended.rules,
+      ...Linter.rules,
       ...ts.configs.recommended.rules,
       // Add your custom rules here
     }
